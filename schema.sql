4d697659992313a559ae89e672bfa10681360b17
Simone Cheah <simone_cheah@hotmail.com>
2020-10-26 03:14:58 +1100

    created initial tables

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE if EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+  
+CREATE TABLE department (
+	id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(30) NOT NULL,
+	PRIMARY KEY (id)
+);
+CREATE TABLE role (
+	id INT AUTO_INCREMENT NOT NULL,
+	title VARCHAR(30) NOT NULL,
+	salary DECIMAL NOT NULL,
+	department_id INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (department_id) REFERENCES department(id)
+);
+CREATE TABLE employee (
+	id INT AUTO_INCREMENT NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	role_id INT NOT NULL,
+	manager_id INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (role_id) REFERENCES role(id),
+	FOREIGN KEY (manager_id) REFERENCES employee(id
+);
