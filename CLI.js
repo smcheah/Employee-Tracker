5125c31c5cb6c191f8e689c821e8f52e5ddc1cf9
Simone Cheah <simone_cheah@hotmail.com>
2020-10-26 04:33:03 +1100

    created init inquirer questions

--- CLI.js
+++ CLI.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const c_table = require('console.table');
+const { nanoid } = require('nanoid'); // user.id = nanoid(10)
 
-const connection = sql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -12,5 +13,33 @@ const connection = sql.createConnection({
 
 connection.connect( err => {
     if (err) throw err;
+    console.log("connected to server, id: " + connection.threadId + "\n");
     init();
 })
+
+function init() {
+    console.log(
+`----------------------------------
+        EMPLOYEE TRACKER 
+----------------------------------`)
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View all',
+                'View all by department id',
+                'View all by role id',
+                'View all by employee id',
+                'Add a new department',
+                'Add a new role',
+                'Add a new employee',
+                'Update employee'
+            ]
+        }
+    ]).then(answer => {
+        console.log(answer.action)
+    })
+}
