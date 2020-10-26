84c0fe80e2af56e1bb478342777c78bf028a38b1
Simone Cheah <simone_cheah@hotmail.com>
2020-10-27 05:07:39 +1100

    fixed inserts

--- seeds.sql
+++ seeds.sql
@@ -6,5 +6,5 @@ INSERT INTO department (name)
 INSERT INTO role (title, salary, department_id)
 	VALUES ("Software Developer", 80000, 1), ("Applications Manager", 150000, 2);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-    VALUES ("George", "Brown", 1, 2), ("Martha", "Newworth", 2, NULL);
+INSERT INTO employee (first_name, last_name, role_id)
+    VALUES ("George", "Brown", 1), ("Martha", "Newworth", 2);
