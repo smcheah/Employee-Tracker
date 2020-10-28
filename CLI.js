1abf4e4331a65fa755a3d971acde4475879e1f1f
Simone Cheah <simone_cheah@hotmail.com>
2020-10-29 03:49:18 +1100

    added add role and department functions

--- CLI.js
+++ CLI.js
@@ -18,10 +18,9 @@ connection.connect( err => {
 })
 
 function init() {
-    console.log(
-`----------------------------------
-        EMPLOYEE TRACKER 
-----------------------------------`)
+    console.log(`
+========== EMPLOYEE TRACKER ==========
+`)
 
     // try {
 
@@ -35,9 +34,10 @@ function init() {
             message: 'What would you like to do?',
             choices: [
                 'View all',
-                'View all by department id',
-                'View all by role id',
-                'View all by employee id',
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'View employees by manager name',
                 'Add a new department',
                 'Add a new role',
                 'Add a new employee',
@@ -50,19 +50,29 @@ function init() {
             case 'View all':
                 viewAll();
                 break;
-            case 'View all by department id':
+            case 'View all departments':
+                viewDepartments();
                 break;
-            case 'View all by role id':
+            case 'View all roles':
+                viewRoles();
                 break;
-            case 'View all by employee id':
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'View employees by manager name':
+                viewByManager(); //
                 break;
             case 'Add a new department':
+                addDepartment();
                 break;
             case 'Add a new role':
+                addRole();
                 break;
             case 'Add a new employee':
+                addEmployee();
                 break;
             case 'Update employee':
+                updateEmployee();
                 break;
             case 'quit':
                 console.log('goodbye!')
@@ -73,18 +83,177 @@ function init() {
                 init();
                 break;
         }
-        
     })
+}
+
+function updateEmployee() {
+    
+}
+function addEmployee() {
+    
+}
+function addRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Input role name:'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Input role salary:'
+        },
+        {
+            type: 'input',
+            name: 'department',
+            message: 'Input department id:'
+        },
+    ]).then(role => {
+        connection.query(
+            `INSERT INTO role(title, salary, department_id)
+            VALUES (?, ?, ?)`,
+            [role.name,
+            role.salary,
+            role.department],
+            function (err, result) {
+                if (err) throw err;
+                console.log(`\nInserted [${role.name}, ${role.salary}, ${role.department}] into role table!`)
+                init();
+            }
+        )
+    })
+}
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Input department name:'
+        }
+    ]).then(department => {
+        connection.query(
+            'INSERT INTO department(name) VALUES (?)',
+            department.name,
+            function (err, result) {
+                if (err) throw err;
+                console.log(`\nInserted ${department.name} into department table!`)
+                init();
+            }
+        )
+    })
+    
+}
+
+async function viewManager() {
+    // get all manager names
+    const results = [];
+
+    connection.query(
+        `SELECT *
+        FROM employee
+        WHERE manager_id is null;`,
+        function (err, result) {
+            if (err) throw err;
+            result.forEach(element => {
+                results.push(element.first_name +' '+ element.last_name)
+            });
+
+            // prompt user with manager names
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'manager',
+                    message: "Which manager's team would you like to view?",
+                    choices: results,
+                }
+            ]).then(answer => {
+                console.log(answer.manager)
+            })
+        }
+    )
+    
+    
+}
+function viewEmployees() {
+    const results = [];
+    connection.query(
+        `SELECT 
+            employee.id,  
+            employee.first_name AS "first name", 
+            employee.last_name AS "last name", 
+            role.title AS position, 
+            role.salary,
+            department.name AS department
+        FROM employee 
+        INNER JOIN role ON (role.id = employee.role_id)
+        INNER JOIN department ON (role.department_id = department.id)
+        ORDER BY employee.id;`,
+        function (err, result) {
+            result.forEach(element => {
+                results.push(element)
+            });
+            console.log("")
+            console.table(results);
+            init();
+        }
+    )
+}
+function viewRoles() {
+    const results = [];
+    connection.query(
+        `SELECT 
+            role.id, 
+            role.title AS role, 
+            role.salary,
+            department.name AS department
+        FROM role 
+        INNER JOIN department ON (role.department_id = department.id)
+        ORDER BY role.id;`,
+        function (err, result) {
+            result.forEach(element => {
+                results.push(element)
+            });
+            console.log("");
+            console.table(results);
+            init();
+        }
+    )
+}
+function viewDepartments() {
+    const results = [];
+    connection.query(
+        `SELECT DISTINCT
+            id, 
+            name AS department
+        FROM department 
+        ORDER BY id;`,
+        function (err, result) {
+            if (err) throw err;
+            result.forEach(element => {
+                results.push(element)
+            });
+            console.log("");
+            console.table(results);
+            init();
+        }
+    )
 }
 
 function viewAll() {
     const results = []
     connection.query(
-        `SELECT 
-            employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name
+        `SELECT
+            employee.id,
+            employee.first_name AS "first name", 
+            employee.last_name AS "last name", 
+            role.title AS position, 
+            role.salary, 
+            department.name AS department
         FROM employee 
         INNER JOIN role ON (role.id = employee.role_id)
-        INNER JOIN department ON (role.department_id = department.id)`,
+        INNER JOIN department ON (role.department_id = department.id)
+        ORDER BY department;`,
         function (err, result) {
             result.forEach(element => {
                 results.push(element)
